f68f3c4e66b14aebd677406c3a2b1c49c72e3a89
irene-np <irene1smr1516@gmail.com>
2019-11-18 18:00:36 +0100

    a

--- nginkApache.sh
+++ nginkApache.sh
@@ -19,7 +19,7 @@ chown www-data:www-data * -R
 
 # configurar el archivo config.php
 cd /var/www/html/iaw-practica-lamp/src/
-sed -i "s/localhost/54.85.90.14/" config.php
+sed -i "s/localhost/35.172.114.64/" config.php
 
 # configurar el archivo php-fpm
 cd /etc/php/7.2/fpm/
@@ -27,13 +27,9 @@ sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" php.ini
 
 # configurar nginx para usar php-fpm
 cd ~
-rm -rf default
-git clone https://github.com/irene-np/default.git
 cp default/default /etc/nginx/sites-available/
 systemctl restart nginx
 
-# Comprobamos
+# configurar nginkx para acceder php
 cd ~
-rm -rf info.php
-git clone https://github.com/irene-np/info.php.git
 cp info.php/info.php  /var/www/html/
